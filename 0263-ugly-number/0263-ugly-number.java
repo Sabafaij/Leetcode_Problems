202697817b6743a8fe91cbf233a202daf4b4b97c
Sabafaij <sabafaij76176@gmail.com>
2026-08-15 02:23:03 +0530

    Time: 1 ms (99.05%), Space: 42.5 MB (69.88%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int n) {
+            while(n%2==0 && n>1){
+                n/=2;
+            }
+            while(n%3==0 && n>1){
+                n/=3;
+            }
+            while(n%5==0 && n>1){
+                n/=5;
+            }
+            if(n==1){
+                return true;
+            }
+        return false;
+    }
+}

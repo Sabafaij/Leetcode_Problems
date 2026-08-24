108ad700d914876f96e49b00569cbe64d190a590
Sabafaij <sabafaij76176@gmail.com>
2026-08-24 14:45:45 +0530

    Time: 21 ms (29.57%), Space: 50.6 MB (90.66%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,20 +1,41 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        int n=matrix.length;
-        if(n*n<k){
-            return 0;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+            int e1r=a[0],e1c=a[1], e2r=b[0], e2c=b[1];
+            int v1=matrix[e1r][e1c];
+            int v2=matrix[e2r][e2c];
+            return v1-v2;
+        });
+        for(int i=0;i<matrix.length;i++){
+            pq.add(new int[]{i,0});
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                pq.offer(matrix[i][j]);
+        while(k-->1){
+            int[] currEleIdx=pq.remove();
+            int cer=currEleIdx[0];
+            int cec=currEleIdx[1];
+            cec++;
+            if(cec<matrix[0].length){
+                pq.add(new int[]{cer,cec});
             }
         }
-        while(k>1){
-            pq.poll();
-            k--;
-        }
-        return pq.peek();
+        int[] ans=pq.remove();
+        int ansR=ans[0];
+        int ansC=ans[1];
+        return matrix[ansR][ansC];
+        // PriorityQueue<Integer> pq=new PriorityQueue<>();
+        // int n=matrix.length;
+        // if(n*n<k){
+        //     return 0;
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         pq.offer(matrix[i][j]);
+        //     }
+        // }
+        // while(k>1){
+        //     pq.poll();
+        //     k--;
+        // }
+        // return pq.peek();
     }
 }

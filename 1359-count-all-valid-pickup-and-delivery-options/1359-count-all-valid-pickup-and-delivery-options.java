24918e0db9be7c42dfac3eefdec5921bacbf7ece
Sabafaij <sabafaij76176@gmail.com>
2026-08-24 11:00:45 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.56%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countOrders(int n) {
+        long mod=1000000007;
+        long ans=1;
+        for(int i=1;i<=n;i++){
+            ans =ans* (2L * i-1);
+            ans=(ans*i)%mod;
+        }
+        return (int)ans;
+    }
+}

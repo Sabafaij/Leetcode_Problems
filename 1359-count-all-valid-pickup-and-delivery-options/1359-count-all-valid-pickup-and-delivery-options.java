32f576b95ae5206c4444780463f7422f17ccc262
Sabafaij <sabafaij76176@gmail.com>
2026-08-24 11:02:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (26.34%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -3,7 +3,7 @@ class Solution {
         long mod=1000000007;
         long ans=1;
         for(int i=1;i<=n;i++){
-            ans =ans* (2L * i-1);
+            ans =ans* (2 * i-1);
             ans=(ans*i)%mod;
         }
         return (int)ans;

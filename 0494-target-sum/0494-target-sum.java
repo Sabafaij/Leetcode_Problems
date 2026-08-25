3d51167d38441b6e7025479bd025a858525e8b61
Sabafaij <sabafaij76176@gmail.com>
2026-08-25 23:50:59 +0530

    Time: 8 ms (59.89%), Space: 46.8 MB (22.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,16 +1,28 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return targetSum(nums,target,0,0);
-    }
-    public int targetSum(int[] nums, int target,int idx,int sum){
-        if(idx==nums.length){
-            if(sum==target){
-                return 1;
-            }
+        int totalSum=0;
+        for(int num:nums){
+            totalSum+=num;
+        }
+        if(totalSum<target){
             return 0;
         }
-        int add=targetSum(nums,target,idx+1,sum+nums[idx]);
-        int sub=targetSum(nums,target,idx+1,sum-nums[idx]);
-        return add+sub;
+        int[][] dp=new int[nums.length+1][2*totalSum+1];
+        for(int[] arr:dp){
+            Arrays.fill(arr,Integer.MIN_VALUE);
+        }
+        return targetSum(nums,target,0,0,totalSum,dp);
+    }
+    public int targetSum(int[] nums, int target,int idx,int sum,int totalSum, int[][] dp){
+        if(idx==nums.length){
+            if(sum==target){
+                return dp[idx][sum+totalSum]=1;
+            }
+            return dp[idx][sum+totalSum]=0;
+        }
+        if(dp[idx][sum+totalSum]!=Integer.MIN_VALUE) return dp[idx][sum+totalSum];
+        int add=targetSum(nums,target,idx+1,sum+nums[idx],totalSum,dp);
+        int sub=targetSum(nums,target,idx+1,sum-nums[idx],totalSum,dp);
+        return dp[idx][sum+totalSum]=add+sub;
     }
 }

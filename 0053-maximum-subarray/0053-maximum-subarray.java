e2758e03970cbddfc160ad641fd07341e1e6119f
Sabafaij <sabafaij76176@gmail.com>
2026-09-02 22:51:44 +0530

    Time: 1 ms (99.94%), Space: 77.3 MB (48.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,8 +1,5 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
         int currMax=nums[0];
         int max=nums[0];
        for(int i=1;i<nums.length;i++){
